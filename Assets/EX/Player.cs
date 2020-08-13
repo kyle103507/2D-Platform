74f341eb17fc126981825d1fb6eefedd0c0e6151
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-08-13 21:43:36 +0800

    場景

---
+++ Assets/EX/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("剛力")]
+    public Rigidbody2D rig;
+    [Header("移動速度")]
+    public float speed = 50f ;
+
+    private void Move()
+    {
+
+    }
+    
+
+
+
+}
